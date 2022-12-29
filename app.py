3e30b8420f609c2a25b420078c2165013dec2f15
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2022-12-29 19:24:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'rockerz2z'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://rockerz2z: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n@github.com/rockerz2z/r2kfilter okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://rockerz2z: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@github.com/rockerz2z/r2kfilter okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
