bfc388cde407ea62bff7c1ad23ecd9bf8eb8f2c7
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2022-12-29 19:53:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-cd /app
-echo "----- Now deployed web booting your repo ------ " 
-gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Eva
+fi
+cd /Eva
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
