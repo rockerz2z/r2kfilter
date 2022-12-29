4593fd53cd694887aafa8e725e677f1bed952c00
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2022-12-29 19:15:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'rockerz2z'
+
+#Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
+os.system("git clone https://rockerz2z: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n@github.com/rockerz2z/r2kfilter okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
